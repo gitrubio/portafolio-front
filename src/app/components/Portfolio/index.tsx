464c1bfdb7e6a80efabbce0a53ec918cbb3d69cc
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-03-24 09:28:44 -0500

    add : section skills

--- src/app/components/Portfolio/index.tsx
+++ src/app/components/Portfolio/index.tsx
@@ -15,15 +15,6 @@ export default function Portfolio( {theme} : IPortfolio) {
     <motion.div className={`container ${theme}`}>
       <Primary theme={theme} img={theme === 'theme-light' ? logo : night}/>
       <Aboutme theme={theme}/>
-
-      <section id="skills">
-        <h2>Section 3</h2>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis aliquid
-        </p>
-        <a href="">Section 1</a>
-        <img src={logo} alt="" />
-      </section>
       <section id="projects">
         <h2>Section 4</h2>
         <p>
---
+++ src/app/components/Portfolio/views/Skills.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Skills() {
+  return (
+    <section id="skills">
+      <h2>Section 3</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis aliquid
+      </p>
+      <a href="">Section 1</a>
+      <img src={''} alt="" />
+    </section>
+  );
+}
