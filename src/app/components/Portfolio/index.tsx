1229010be0938e50b6518e2ee42b5c148dddc02e
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-03-29 22:09:25 -0500

    add : mejoras y correpcion de errores

--- src/app/components/Portfolio/index.tsx
+++ src/app/components/Portfolio/index.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./css/Portfolio.css";
 import logo from "./assets/develop.svg";
+import me from "../../../assets/yo_prueba.jpeg";
 import night from "./assets/night.svg";
 import Primary from "./views/Presentation";
 import { motion } from "framer-motion";
 
 import { IPortfolio } from "../../interfaces/Portfolio.interface";
 import Aboutme from "./views/Aboutme";
+import Skills from "./views/Skills";
 
 
 export default function Portfolio( {theme} : IPortfolio) {
 
   return (
     <motion.div className={`container ${theme}`}>
-      <Primary theme={theme} img={theme === 'theme-light' ? logo : night}/>
-      <Aboutme theme={theme}/>
+      <Primary key='Primary' theme={theme} img={theme === 'theme-light' ? logo : night}/>
+      <Aboutme key='Aboutme' theme={theme} img={me}/>
+      <Skills  key='Skills'/>
       <section id="projects">
         <h2>Section 4</h2>
         <p>
--- src/app/components/Portfolio/views/Aboutme.tsx
+++ src/app/components/Portfolio/views/Aboutme.tsx
@@ -1,38 +1,56 @@
 import React, { useState } from "react";
 import "../css/aboutMe.css";
 import { motion } from "framer-motion";
-import AnimatedText from '../../animated/AnimatedText';
+import AnimatedText from "../../animated/AnimatedText";
 import { ISection } from "../../../interfaces/Portfolio.interface";
 
-export default function Aboutme( props? : ISection) {
+export default function Aboutme(props?: ISection) {
   const [rotate, setRotate] = useState(30);
 
   return (
-    <section >
+    <section>
       <div className="aboutMe">
+        {props ? (
+          <motion.div
+            style={{ width: 200, height: 100, margin: "auto" }}
+            initial={{ scale: 0 }}
+            whileInView={{ rotate: 360, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 40,
+            }}
+          >
+            <motion.img src={props.img} style={{ width: 100 }} />
+          </motion.div>
+        ) : (
+          <div></div>
+        )}
         <motion.h1 className="text" style={{ textAlign: "center" }}>
-          Hi
-          {
-            <motion.h1
-              className="hello"
-              whileHover={{ rotate: [0, 15, 0, 15, 0] }}
-              whileInView={{ rotate: [0, 15, 0, 15, 0] }}
-              transition={{ type: "spring", duration: 2 }}
-            >
-              👋
-            </motion.h1>
-          }{" "}
-          , I'm Carlos Rubio Viloria
+          Hi,
         </motion.h1>
-        <motion.div 
-        className="description"
-        initial={{top : -100}}
-        whileInView={{top: 100}}
-        transition={{ type: "spring", duration: 2 }}
+        <motion.h1
+          className="hello"
+          whileHover={{ rotate: [0, 15, 0, 15, 0] }}
+          whileInView={{ rotate: [0, 15, 0, 15, 0] }}
+          transition={{ type: "spring", duration: 2 }}
         >
-         <AnimatedText line=" I help designers, agencies and companies bring their ideas to life.
+          👋
+        </motion.h1>
+        <motion.h1 className="text" style={{ textAlign: "center" }}>
+          I'm Carlos Rubio Viloria
+        </motion.h1>
+        <motion.div
+          className="description"
+          initial={{ top: -100 }}
+          whileInView={{ top: 100 }}
+          transition={{ type: "spring", duration: 2 }}
+        >
+          <AnimatedText
+            line=" I help designers, agencies and companies bring their ideas to life.
           with the creation of dynamic pages, landing pages, dashboards,
-          eCommerce pages, microservices, Rest API."/>
+          eCommerce pages, microservices, Rest API."
+          />
         </motion.div>
       </div>
     </section>
