515b0249f57b531d94b5358b10cf8c8e7e83e53f
gitRubio <carloviloria0@gmail.com>
2023-10-26 12:44:22 -0500

    add : delete fields

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,3 @@
-import develop from '../../assets/develop.svg'
 
 export const THEME = {
     LIGHT: 'theme-light',
@@ -6,7 +5,7 @@ export const THEME = {
 }
 
 export const BACK_GROUND = {
-    LIGHT: develop,
+    LIGHT: "https://firebasestorage.googleapis.com/v0/b/e-comerce-f3c71.appspot.com/o/develop.svg?alt=media&token=2dba35c5-30d6-47f1-a4f6-b63784631c7f",
     DARK: "https://firebasestorage.googleapis.com/v0/b/e-comerce-f3c71.appspot.com/o/night.svg?alt=media&token=376baa9b-60fd-4562-ba26-12350b19f9a4"
 }
 
